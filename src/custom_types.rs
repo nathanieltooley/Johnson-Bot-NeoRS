9add02434e3624827ed81da8e54846243970500f
nathanieltooley <gotablast99@gmail.com>
2024-01-25 22:01:02 -0600

    Add db handle to data struct and new user schema struct

--- src/custom_types.rs
+++ src/custom_types.rs
@@ -1,7 +1,32 @@
-#[derive(Debug)]
-// Custom data to send between commands
-pub struct Data {}
-// Custom error type alias that is an Error that implements Send and Sync (for async stuff)
-pub type Error = Box<dyn std::error::Error + Send + Sync>;
-// Poise context constructed with custom Data and Error types
-pub type Context<'a> = poise::Context<'a, Data, Error>;
+pub mod command {
+    use mongodb::Database;
+
+    #[derive(Debug)]
+    // Custom data to send between commands
+    pub struct Data {
+        pub johnson_handle: Database,
+    }
+    // Custom error type alias that is an Error that implements Send and Sync (for async stuff)
+    pub type Error = Box<dyn std::error::Error + Send + Sync>;
+    // Poise context constructed with custom Data and Error types
+    pub type Context<'a> = poise::Context<'a, Data, Error>;
+}
+
+pub mod mongo_schema {
+    use mongodb::bson::{DateTime, Document};
+    use poise::serenity_prelude::UserId;
+    use serde::{Deserialize, Serialize};
+
+    #[derive(Debug, Serialize, Deserialize)]
+    pub struct User {
+        name: String,
+        discord_id: UserId,
+        date_created: DateTime,
+        vbucks: u32,
+        exp: u32,
+        level: u16,
+        slur_count: Option<Document>,
+        inventory: Option<Vec<Document>>,
+        stroke_count: Option<u32>,
+    }
+}
