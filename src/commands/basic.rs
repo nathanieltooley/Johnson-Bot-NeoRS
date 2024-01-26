914605aa31d2ed4950201f68af1dcb24496a0882
nathanieltooley <gotablast99@gmail.com>
2024-01-25 22:01:16 -0600

    Add some mongo test code to command

--- src/commands/basic.rs
+++ src/commands/basic.rs
@@ -1,10 +1,19 @@
-use crate::custom_types::{Context, Error};
-use tracing::{info, instrument};
+use crate::custom_types::command::{Context, Error};
+use crate::mongo::get_users;
+use futures::stream::TryStreamExt;
+use mongodb::bson::doc;
+use tracing::{debug, info, instrument};
 
 #[poise::command(slash_command, prefix_command)]
-#[instrument(name = "ping", skip_all, fields(data = ?ctx.data()))]
+#[instrument(name = "ping", skip_all)]
 pub async fn ping(ctx: Context<'_>) -> Result<(), Error> {
     ctx.say("Ping!").await?;
+
+    for user in get_users(&ctx).await? {
+        debug!("User: {:?}", user);
+    }
+
     info!("Johnson pinged {}", ctx.author().name);
+
     Ok(())
 }
