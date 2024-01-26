0431d1474f1991737c6ba6600c68a99dd3f322e9
nathanieltooley <gotablast99@gmail.com>
2024-01-25 22:01:34 -0600

    Add code for setting up mongo

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@ mod commands;
 mod custom_types;
 mod events;
 mod logging;
+mod mongo;
 
+use mongodb::Database;
 use poise::serenity_prelude::{self as serenity, GuildId};
 use poise::Command;
 
-use custom_types::{Data, Error};
+use custom_types::command::{Data, Error};
 use events::Handler;
 
 #[allow(dead_code)]
@@ -20,13 +22,16 @@ impl<'a> CommandRegistering {
         &self,
         ctx: &serenity::Context,
         commands: &[Command<Data, Error>],
+        j_handle: Database,
     ) -> Result<Data, Box<dyn std::error::Error + Sync + Send>> {
         //
         match self {
             // Register the commands globally
             CommandRegistering::Global => {
                 poise::builtins::register_globally(ctx, commands).await?;
-                Ok(Data {})
+                Ok(Data {
+                    johnson_handle: j_handle,
+                })
             }
             // Register commands for every provided guild
             CommandRegistering::ByGuild(guilds) => {
@@ -34,7 +39,9 @@ impl<'a> CommandRegistering {
                     // Deref and copy guild_id
                     poise::builtins::register_in_guild(ctx, commands, *guild).await?;
                 }
-                Ok(Data {})
+                Ok(Data {
+                    johnson_handle: j_handle,
+                })
             }
         }
     }
@@ -46,13 +53,20 @@ async fn main() {
     logging::log_init();
 
     // Configuration
-    let d_token =
+    let token =
         std::env::var("TOKEN").expect("Johnson should be able to find TOKEN environment var");
     let intents = serenity::GatewayIntents::non_privileged();
     let opts = poise::FrameworkOptions {
         commands: vec![commands::basic::ping()],
         ..Default::default()
     };
+
+    // MongoDB setup
+    let mongo_host = std::env::var("DISCORD_HOST")
+        .expect("Johnson should be able to find MONGO_HOST environment var");
+    let client = mongo::receive_client(&mongo_host)
+        .await
+        .expect("Johnson should be able to get MongoDB client");
 
     // Set register type
     let registering = CommandRegistering::ByGuild(vec![GuildId::new(427299383474782208)]);
@@ -65,14 +79,18 @@ async fn main() {
             // Requires a pin that holds a future
             Box::pin(async move {
                 registering
-                    .register(ctx, &framework.options().commands)
+                    .register(
+                        ctx,
+                        &framework.options().commands,
+                        client.database("Johnson"),
+                    )
                     .await
             })
         })
         .build();
 
     // Build client
-    let mut client = serenity::ClientBuilder::new(d_token, intents)
+    let mut client = serenity::ClientBuilder::new(token, intents)
         .framework(framework)
         .event_handler(Handler)
         .await
