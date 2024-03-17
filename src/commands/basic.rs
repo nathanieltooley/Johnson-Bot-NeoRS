8be5bf032ab16ebe0395b3184fa15e9d298c84df
nathanieltooley <gotablast99@gmail.com>
2024-03-17 03:07:36 -0500

    Added latency to Ping reply

--- src/commands/basic.rs
+++ src/commands/basic.rs
@@ -1,10 +1,11 @@
-use crate::custom_types::command::{Context, Error};
+use crate::custom_types::command::{Context, Data, Error};
 use tracing::{debug, info, instrument};
 
 #[poise::command(slash_command, prefix_command)]
 #[instrument(name = "ping", skip_all)]
 pub async fn ping(ctx: Context<'_>) -> Result<(), Error> {
-    ctx.say("Ping!").await?;
+    ctx.say(format!("Ping! {} ms", ctx.ping().await.as_millis()))
+        .await?;
 
     info!("Johnson pinged {}", ctx.author().name);
 
