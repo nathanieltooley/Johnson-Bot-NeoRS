6b4bb4da6530ad6c516ab49965a690fe73a04c25
nathanieltooley <gotablast99@gmail.com>
2024-03-26 19:44:28 -0500

    Mostly finished RPS

--- src/commands/gamble.rs
+++ src/commands/gamble.rs
@@ -1,17 +1,20 @@
 use poise::serenity_prelude::{
-    self, ComponentInteractionDataKind, CreateButton, CreateInteractionResponse,
+    self, ComponentInteractionDataKind, Content, CreateButton, CreateInteractionResponse,
     CreateInteractionResponseMessage, CreateMessage, Mentionable, Message, UserId,
 };
+use poise::CreateReply;
 use serenity_prelude::futures::StreamExt;
 use std::collections::HashMap;
 use std::str::FromStr;
 use std::time::Duration;
 use tokio::time::sleep;
-use tracing::{debug, info};
+use tracing::{debug, info, instrument};
 
 use crate::custom_types::command::{Context, Error};
+use crate::events::error_handle;
+use crate::mongo;
 use crate::utils::message::interaction::wait_for_user_interaction;
-use crate::utils::message::{send_channel_message, simple_message};
+use crate::utils::message::{simple_channel_message, simple_message};
 
 #[derive(Hash, PartialEq, Eq, Clone, Copy, Debug)]
 enum Rps {
@@ -45,6 +48,7 @@ async fn get_participant_choice(
     rps_m: &Message,
     comp_id: UserId,
 ) -> Option<Rps> {
+    info!("Waiting for RPS interaction");
     let mut rps_stream = rps_m
         .await_component_interaction(ctx)
         .timeout(Duration::from_secs(60))
@@ -54,6 +58,7 @@ async fn get_participant_choice(
         let user_id = interaction.user.id;
 
         if user_id != comp_id {
+            info!("{user_id} attempted interaction with RPS choice message");
             let _ = interaction
                 .create_response(
                     ctx,
@@ -67,6 +72,7 @@ async fn get_participant_choice(
         } else {
             match interaction.data.kind {
                 ComponentInteractionDataKind::Button => {
+                    info!("Got RPS choice interaction!");
                     // Store in var just in case a problem comes from deleting the message before
                     // hand
                     let temp = Rps::from_str(&interaction.data.custom_id).ok();
@@ -85,22 +91,26 @@ async fn get_participant_choice(
     None
 }
 
-#[poise::command(slash_command, prefix_command)]
+#[instrument(skip_all)]
+#[poise::command(slash_command, on_error = "error_handle")]
 pub async fn rock_paper_scissors(
     ctx: Context<'_>,
     #[description = "Who you're challenging"] opponent: serenity_prelude::User,
+    #[description = "What you're willing to wager"] wager: u64,
 ) -> Result<(), Error> {
     if ctx.guild_id().is_none() {
         return Ok(());
     }
 
-    let temp_messages: Vec<Message> = vec![];
+    debug!("Getting command interaction");
+    let cmd_interaction = match ctx {
+        Context::Application(a) => a.interaction,
+        _ => panic!("impossible"),
+    };
 
-    ctx.defer_ephemeral().await?;
-
+    let mongo_driver = mongo::ContextWrapper::new_slash(ctx);
     let guild_id = ctx.guild_id().unwrap();
     let author = ctx.author();
-
     let author_nick = author
         .nick_in(ctx, guild_id)
         .await
@@ -111,17 +121,97 @@ pub async fn rock_paper_scissors(
         .await
         .unwrap_or(opponent.name.clone());
 
+    let rps_message_comp_auth = CreateMessage::new()
+        .content(format!("Choose a Move {}!", author_nick))
+        .button(CreateButton::new("rock").label("Rock"))
+        .button(CreateButton::new("paper").label("Paper"))
+        .button(CreateButton::new("scissors").label("Scissors"));
+
+    let rps_message_comp_op = CreateMessage::new()
+        .content(format!("Choose a Move {}!", opponent_nick))
+        .button(CreateButton::new("rock").label("Rock"))
+        .button(CreateButton::new("paper").label("Paper"))
+        .button(CreateButton::new("scissors").label("Scissors"));
+
+    let win_table: HashMap<Rps, HashMap<Rps, RpsResult>> = HashMap::from([
+        (
+            Rps::Rock,
+            HashMap::from([
+                (Rps::Rock, RpsResult::Tie),
+                (Rps::Paper, RpsResult::Loss),
+                (Rps::Scissors, RpsResult::Win),
+            ]),
+        ),
+        (
+            Rps::Paper,
+            HashMap::from([
+                (Rps::Rock, RpsResult::Win),
+                (Rps::Paper, RpsResult::Tie),
+                (Rps::Scissors, RpsResult::Loss),
+            ]),
+        ),
+        (
+            Rps::Scissors,
+            HashMap::from([
+                (Rps::Rock, RpsResult::Loss),
+                (Rps::Paper, RpsResult::Win),
+                (Rps::Scissors, RpsResult::Tie),
+            ]),
+        ),
+    ]);
+
+    let author_mongo_user = mongo_driver.get_user_safe(&author).await?;
+    let opponent_mongo_user = mongo_driver.get_user_safe(&opponent).await?;
+
+    debug!("Checking for author money");
+    if TryInto::<u64>::try_into(author_mongo_user.vbucks).unwrap() < wager {
+        cmd_interaction
+            .create_response(
+                ctx,
+                CreateInteractionResponse::Message(
+                    CreateInteractionResponseMessage::new()
+                        .content(format!("You do not have enough money for this wager!"))
+                        .ephemeral(true),
+                ),
+            )
+            .await?;
+
+        return Ok(());
+    }
+
+    debug!("Checking for opponent money");
+    if TryInto::<u64>::try_into(opponent_mongo_user.vbucks).unwrap() < wager {
+        cmd_interaction
+            .create_response(
+                ctx,
+                CreateInteractionResponse::Message(
+                    CreateInteractionResponseMessage::new()
+                        .content(format!(
+                            "{opponent} does not have enough money for this wager!"
+                        ))
+                        .ephemeral(true),
+                ),
+            )
+            .await?;
+
+        return Ok(());
+    }
+
+    ctx.defer_ephemeral().await?;
+
+    debug!("Sending acceptance message");
     let accept_message = ctx
         .channel_id()
         .send_message(
             ctx,
             CreateMessage::new()
-                .content(format!("{}, {author_nick} challenges you to a Rock Paper Scissors Duel. Do you accept?", opponent.mention()))
+                .content(format!("{}, {author_nick} challenges you to a Rock Paper Scissors Duel, putting ${wager} on the line. Do you accept?", opponent.mention()))
                 .button(CreateButton::new("accept").label("Accept"))
                 .button(CreateButton::new("decline").label("Decline")),
         )
         .await?;
 
+    info!("Waiting for interaction from acceptance message");
     let accept_interaction =
         wait_for_user_interaction(&ctx, &accept_message, opponent.id, Duration::from_secs(60))
             .await;
@@ -130,6 +220,7 @@ pub async fn rock_paper_scissors(
         Some(int) => match int.data.kind {
             ComponentInteractionDataKind::Button => {
                 if int.data.custom_id.as_str() == "decline" {
+                    info!("{opponent} declined RPS challenge");
                     ctx.channel_id()
                         .send_message(
                             ctx,
@@ -144,6 +235,7 @@ pub async fn rock_paper_scissors(
 
                     return Ok(());
                 } else {
+                    info!("{opponent} accepted RPS challenge");
                     ctx.channel_id()
                         .send_message(
                             ctx,
@@ -173,57 +265,10 @@ pub async fn rock_paper_scissors(
         }
     }
 
+    debug!("Deleting acceptance message");
     let _ = accept_message.delete(ctx).await;
 
-    let win_table: HashMap<Rps, HashMap<Rps, RpsResult>> = HashMap::from([
-        (
-            Rps::Rock,
-            HashMap::from([
-                (Rps::Rock, RpsResult::Tie),
-                (Rps::Paper, RpsResult::Loss),
-                (Rps::Scissors, RpsResult::Win),
-            ]),
-        ),
-        (
-            Rps::Paper,
-            HashMap::from([
-                (Rps::Rock, RpsResult::Win),
-                (Rps::Paper, RpsResult::Tie),
-                (Rps::Scissors, RpsResult::Loss),
-            ]),
-        ),
-        (
-            Rps::Scissors,
-            HashMap::from([
-                (Rps::Rock, RpsResult::Loss),
-                (Rps::Paper, RpsResult::Win),
-                (Rps::Scissors, RpsResult::Tie),
-            ]),
-        ),
-    ]);
-
-    // Create the Message Component
-    let rps_message_comp_auth = CreateMessage::new()
-        .content(format!(
-            "Choose a Move {}!",
-            // Get the Author's nickname
-            author_nick
-        ))
-        .button(CreateButton::new("rock").label("Rock"))
-        .button(CreateButton::new("paper").label("Paper"))
-        .button(CreateButton::new("scissors").label("Scissors"));
-
-    let rps_message_comp_op = CreateMessage::new()
-        .content(format!(
-            "Choose a Move {}!",
-            // Get the Author's nickname
-            opponent_nick
-        ))
-        .button(CreateButton::new("rock").label("Rock"))
-        .button(CreateButton::new("paper").label("Paper"))
-        .button(CreateButton::new("scissors").label("Scissors"));
-
-    // Send the Message
+    info!("Sending RPS choice message for author");
     let rps_m = ctx
         .channel_id()
         .send_message(&ctx, rps_message_comp_auth)
@@ -237,7 +282,7 @@ pub async fn rock_paper_scissors(
         }
     };
 
-    // Send another message to the opponent
+    info!("Sending RPS choice message for opponent");
     let rps_m = ctx
         .channel_id()
         .send_message(&ctx, rps_message_comp_op)
@@ -262,7 +307,7 @@ pub async fn rock_paper_scissors(
         .get(&opponent_choice)
         .unwrap();
 
-    send_channel_message(
+    simple_channel_message(
         &ctx,
         format!(
             "{} chose {:?}, while {} chose {:?}!",
@@ -274,12 +319,16 @@ pub async fn rock_paper_scissors(
 
     match result {
         RpsResult::Win => {
-            send_channel_message(&ctx, format!("{} Wins!", ctx.author().mention()).as_str())
+            simple_channel_message(&ctx, format!("{} Wins!", ctx.author().mention()).as_str())
                 .await?;
             // You Win!
+            debug!("Attempting to give winnings to author");
+            mongo_driver
+                .player_transaction(&opponent, &author, wager)
+                .await?;
         }
         RpsResult::Tie => {
-            send_channel_message(
+            simple_channel_message(
                 &ctx,
                 format!(
                     "{} and {} Tied :(",
@@ -292,17 +341,14 @@ pub async fn rock_paper_scissors(
             // No one wins :(
         }
         RpsResult::Loss => {
-            send_channel_message(&ctx, format!("{} Wins! :((", opponent.mention()).as_str())
+            simple_channel_message(&ctx, format!("{} Wins! :((", opponent.mention()).as_str())
+                .await?;
+            debug!("Attempting to give winnings to opponent");
+            mongo_driver
+                .player_transaction(&author, &opponent, wager)
                 .await?;
             // You lose :((
         }
-    }
-
-    sleep(Duration::from_secs(5)).await;
-
-    info!("Deleting temp messages for RPS");
-    for m in temp_messages {
-        m.delete(ctx).await?;
     }
 
     Ok(())
