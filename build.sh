ca6628d67aa5f6193dddb00b076d878c9c1c8076
nathanieltooley <gotablast99@gmail.com>
2024-08-31 02:03:46 -0500

    Build script now auto triggers build on remote server

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-docker build . -t beanbubger/johnson-rs:latest
-docker image push beanbubger/johnson-rs:latest
+image_name=beanbubger/johnson-rs:latest
+
+docker build . -t $image_name 
+docker image push $image_name
+
+image_digest="$(docker inspect --format='{{index .RepoDigests 0}}' $image_name)"
+
+ssh root@$HOST_IP docker system prune -a -f
+ssh root@$HOST_IP dokku git:from-image johnson-rs $image_digest
