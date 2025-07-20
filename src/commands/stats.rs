0040bcd0ecd2a55fad2e2f2cc1664c9d3f5d5c1e
Nathaniel Tooley <gotablast99@gmail.com>
2025-07-19 22:41:44 -0500

    feat(command): show stats command can now show other people's stats

--- src/commands/stats.rs
+++ src/commands/stats.rs
@@ -1,3 +1,4 @@
+use poise::serenity_prelude::User;
 use poise::CreateReply;
 
 use crate::custom_types::command::{Context, Error};
@@ -9,9 +10,12 @@ use crate::utils::message::embed::base_embed;
 pub async fn show_stats(
     ctx: Context<'_>,
     #[description = "Let the whole world know?"] annoy_others: bool,
+    #[description = "Who's stats would you like to see. By default this is you."]
+    user_to_show: Option<User>,
 ) -> Result<(), Error> {
     let db_handler = Database::new(ctx);
-    let user_info = db_handler.get_user(ctx.author()).await?;
+    let user = user_to_show.unwrap_or_else(|| ctx.author().to_owned());
+    let user_info = db_handler.get_user(&user).await?;
 
     let stat_fields = vec![
         ("XP", format!("*{}*", user_info.exp), false),
@@ -19,7 +23,7 @@ pub async fn show_stats(
     ];
 
     let stats_embed = base_embed()
-        .title(format!("{}'s Stats", ctx.author().name))
+        .title(format!("{}'s Stats", user.name))
         .fields(stat_fields);
 
     ctx.send(
