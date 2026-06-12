74b59e775fdc08b0ea0d98137998838f13d2a036
Nathaniel Tooley <gotablast99@gmail.com>
2026-06-11 23:44:25 -0500

    fix(friends): update friendships table to have composite key

--- migrations/003_jb.sql
+++ migrations/003_jb.sql
@@ -1,4 +1,6 @@
 create table if not exists friendships (
     user_from integer not null,
-    user_to integer not null
+    user_to integer not null,
+    relation_type integer not null,
+    PRIMARY KEY (user_from, user_to)
 );
